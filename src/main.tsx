ca432823f759a252e5f70d4c4f217277e12af656
ifeoluwafolami <folamihephzibah@gmail.com>
2025-10-18 21:53:28 +0100

    font issue fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
-import '@fontsource/rock-salt';
-import '@fontsource/montserrat'; 
+ 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
